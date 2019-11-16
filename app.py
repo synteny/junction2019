3a1397a8f2db86d57cc14ddd7aae7572c6e48e53
Vladimir Smirnov <synteny@users.noreply.github.com>
2019-11-16 14:41:03 +0300

    added method /recipes?query=<searchString> to search Kesko API by substring

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
@@ -8,5 +9,20 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/recipes')
+def recipies():
+    url = 'https://kesko.azure-api.net/v1/search/recipes'
+
+    data = '''{
+        "query": "%s"
+    }''' % request.args.get("query")
+
+    response = requests.post(url, data=data, headers={
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": "c3adbde35d0a40d58e6bc1c99751c129"
+    })
+    return response.content
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, port=5000)
