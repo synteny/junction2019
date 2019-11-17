bb3dd1d021a502696428fedd3b4184719bdc8eed
Vladimir Smirnov <synteny@users.noreply.github.com>
2019-11-17 07:20:40 +0300

    Parameterized Jinja template with dummy arguments

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def render():
-    return render_template('html/food-search.html')
+    recipes = [
+        {'title': 'Item 1', 'categories': ['cat 1', 'cat 2'], 'ingredients': ['item 1', 'item 2'],
+         'img': 'https://k-file-storage-qa.imgix.net/f/k-ruoka/recipe/6832?w=1000&h=1000&fit=clip'},
+        {'title': 'Item 2', 'categories': ['cat 1', 'cat 2'], 'ingredients': ['item 1', 'item 2'],
+         'img': 'https://k-file-storage-qa.imgix.net/f/k-ruoka/recipe/6832?w=1000&h=1000&fit=clip'}
+    ]
+
+    return render_template('foodremix.html', recipes=recipes if request.form else None)
 
 
 if __name__ == '__main__':
